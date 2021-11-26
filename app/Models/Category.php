5fe25ef71719e31d6e5c2fc85dddc5b96422cd1b
cheems-dev <daprimovaria@gmail.com>
2021-11-26 09:56:55 -0500

    chore: add models :beer:

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,18 +2,20 @@
 
 namespace App\Models;
 
+use App\Traits\ApiTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, ApiTrait;
 
     protected $fillable = [
         'name',
         'slug',
     ];
-
+    protected $allowIncluded = ['posts', 'posts.user'];
+    protected $allowFilter, $allowSort = ['id', 'name', 'slug'];
     public function posts()
     {
         return $this->hasMany(Post::class);
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\ApiTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, ApiTrait;
     const PUBLISHED = 1, MODERATION = 2;
     protected $fillable = ['name', 'slug', 'extract', 'body', 'status', 'category_id', 'user_id'];
     /**
