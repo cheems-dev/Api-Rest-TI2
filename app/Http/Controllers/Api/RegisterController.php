47e9b5a1ddec756baf1f6350e2f457bb9f468d1f
cheems-dev <daprimovaria@gmail.com>
2021-11-26 09:56:01 -0500

    chore: add controller

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -5,15 +5,20 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\User;
+
 class RegisterController extends Controller
 {
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|string|min:8|confirmed'
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            /* 'password' => 'required|string|min:8|confirmed' */
         ]);
-        return $request;
+
+        $user = User::create($request->all());
+
+        return response($user, 200);
     }
 }
